302b880292aae0d0af1c53a4ed2f523e1ef8f43b
Ganshir <vincent.dobremel@gmail.com>
2019-03-18 14:58:48 +0100

    MaJ Site

--- src/bean/Site.java
+++ src/bean/Site.java
@@ -2,4 +2,32 @@ package bean;
 
 public class Site {
 
+		
+	private int idSite;
+	private String nom;
+	
+	
+	//Constructeurs
+	public Site(String nom) {
+		this.nom = nom;
+	}
+
+	public Site() {
+	}
+	
+	
+	//Ascesseur, Mutateurs
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public int getIdSite() {
+		return idSite;
+	}
+	
+	
 }
