d2bb53c4309ebcb890f510b40bf1361819373788
Ganshir <vincent.dobremel@gmail.com>
2019-03-21 14:33:22 +0100

    Classe Helper Hashage & isValide
    Hashage du mdp  et  verification que le mdp soit valide

--- src/dao/TestJava.java
+++ src/dao/TestJava.java
@@ -2,16 +2,26 @@ package dao;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * @author DWWM
+ *
+ */
 public class TestJava {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		
-		String mp1 = DaoHelper.hash("antoine");
-		String mp2 = DaoHelper.hash("eniotna");    
+		String mp1 = "012345678910123456789";
+		String mp2;
 		
-		System.out.println(mp1);
-		System.out.println(mp2);
+		Pattern p = Pattern.compile("\\d{8,15}+");
+		Matcher m = p.matcher(mp1);
+
+		boolean bool = m.matches();
+		
+		System.out.println(bool);
 	}
 }	
 		
