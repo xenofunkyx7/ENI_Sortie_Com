21c13b0a1e88b4ba1fcb4b48f784a6af14f09ea3
xenofunkyx7 <mel.85.15@gmail.com>
2019-03-26 13:49:40 +0100

    filtre à air de compet'

---
+++ src/filtre/FiltreMembre.java
@@ -0,0 +1,35 @@
+package filtre;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/membre/*")
+public class FiltreMembre implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		HttpSession session = req.getSession();
+		if (session.getAttribute("utilisateur") != null) {
+			chain.doFilter(request, response);
+		}else {
+			resp.sendRedirect("../login");
+		}
+		
+		
+		
+	}
+
+}
